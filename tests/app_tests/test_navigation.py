65abad0712415a2e947d685674c6e246873db166
Zaloog <gramslars@gmail.com>
2025-05-06 17:01:31 +0200

    skip windows app test

--- tests/app_tests/test_navigation.py
+++ tests/app_tests/test_navigation.py
@@ -1,3 +1,7 @@
+import sys
+
+import pytest
+
 from ayu.app import AyuApp
 
 # from ayu.event_dispatcher import check_connection
@@ -10,6 +14,7 @@ from ayu.app import AyuApp
 #         assert pilot.app.port == test_port
 
 
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="Windows is too slow")
 async def test_app_screen(test_app: AyuApp, test_port, test_host):
     async with test_app.run_test() as pilot:
         assert pilot.app.test_path.as_posix() == "tests/test_cases"
@@ -19,6 +24,6 @@ async def test_app_screen(test_app: AyuApp, test_port, test_host):
         assert pilot.app.port == test_port
 
         # Wait for test collection
-        await pilot.pause(30)
+        await pilot.pause(5)
 
         assert pilot.app.data_test_tree
