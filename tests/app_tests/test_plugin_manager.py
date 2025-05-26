2fa3d702fc267e80d55514ca3037bdc3694ff472
Zaloog <gramslars@gmail.com>
2025-05-26 11:43:24 +0200

    Remove Loadgroups for xdist testing

--- tests/app_tests/test_plugin_manager.py
+++ tests/app_tests/test_plugin_manager.py
@@ -7,7 +7,7 @@ from ayu.app import AyuApp
 from ayu.widgets.modals.plugin_manager import ModalPlugin
 
 
-@pytest.mark.xdist_group(name="group2")
+# @pytest.mark.xdist_group(name="group2")
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Windows is too slow")
 async def test_app_plugins(testcase_path):
     os.environ["AYU_PORT"] = "1339"
@@ -16,6 +16,7 @@ async def test_app_plugins(testcase_path):
     async with test_app.run_test() as pilot:
         # Wait for test collection
         await pilot.press("P")
+        await pilot.pause(2)
         assert isinstance(pilot.app.screen, ModalPlugin)
 
         for plugin in ["ayu", "asyncio", "xdist", "pytest_cov"]:
